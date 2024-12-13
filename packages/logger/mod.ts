5e1ac53515328837e8c21df5092fde8399c79c4b
neonbyte1 <186292855+neonbyte1@users.noreply.github.com>
2024-12-13 18:21:30 +0100

    📝 logger: added very basic module description

--- packages/logger/mod.ts
+++ packages/logger/mod.ts
@@ -1,4 +1,16 @@
+/**
+ * @module
+ *
+ * Colorful logger inspired by Nest.
+ *
+ * @example
+ * ````ts
+ * import { Logger } from "jsr:@denorid/logger";
+ *
+ * Logger.log("Hello, world!");
+ * ```
+ */
 export * from "./level.ts";
 export * from "./logger-service.ts";
-export * from "./options.ts";
 export * from "./logger.ts";
+export * from "./options.ts";
