66678a272ce7035986e0b65adc42151755974d59
neonbyte1 <186292855+neonbyte1@users.noreply.github.com>
2026-02-28 14:13:41 +0000

    feat(core/http): added internal constants

---
+++ packages/core/src/http/_constants.ts
@@ -0,0 +1,3 @@
+export const CONTROLLER_METADATA = Symbol.for("denorid.controller");
+export const HTTP_CONTROLLER_METADATA = Symbol.for("denorid.http_controller");
+export const CONTROLLER_REQUEST_MAPPING = Symbol.for("denorid.request_mapping");
