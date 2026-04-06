8f00dc67e50eb6a76617e13fae9ec8ec4161f46d
neonbyte1 <186292855+neonbyte1@users.noreply.github.com>
2026-04-06 16:30:15 +0000

    feat(core): added constant values

---
+++ packages/core/_constants.ts
@@ -0,0 +1,8 @@
+export const CONTROLLER_METADATA = Symbol.for("denorid.controller");
+export const HTTP_CONTROLLER_METADATA = Symbol.for("denorid.http_controller");
+export const CONTROLLER_REQUEST_MAPPING = Symbol.for("denorid.request_mapping");
+
+export const EXCEPTION_FILTER = Symbol.for("denorid.exception_filter");
+export const EXCEPTION_FILTER_TARGET = Symbol.for(
+  "denorid.exception_filter.target",
+);
