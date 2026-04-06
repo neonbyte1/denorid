313ffdcef13cedff036ba4a3ffc0cd31c04482d8
neonbyte1 <186292855+neonbyte1@users.noreply.github.com>
2026-04-06 18:41:33 +0000

    feat(core): added in-flight host arguments interface

---
+++ packages/core/host_arguments.ts
@@ -0,0 +1,34 @@
+/**
+ * Methods to obtain request and response objects.
+ */
+export interface HttpHostArguments {
+  /**
+   * Returns the in-flight `request` object.
+   *
+   * @template {unknown} T
+   *
+   * @returns {T} The request object, where the actual type depends on the underlying
+   * implementation.
+   */
+  getRequest<T = unknown>(): T;
+
+  /**
+   * Returns the in-flight `response` object.
+   *
+   * @template {unknown} T
+   *
+   * @returns {T} The response object, where the actual type depends on the underlying
+   * implementation. For example: hono doesn't have a "Response" interface, because
+   * the so called `Context` holds the response and methods to set or modify the response.
+   *
+   * Use this function with caution.
+   */
+  getResponse<T = unknown>(): T;
+}
+
+/**
+ * Provides methods for retrieving the arguments being passed to a handler.
+ */
+export interface HostArguments {
+  switchToHttp(): HttpHostArguments;
+}
