17625ba01fc801399acfe8a59f57ab671c48d570
neonbyte1 <186292855+neonbyte1@users.noreply.github.com>
2026-04-06 16:29:50 +0000

    chore: cleanup

--- packages/core/src/http/_constants.ts
+++
@@ -1,3 +0,0 @@
-export const CONTROLLER_METADATA = Symbol.for("denorid.controller");
-export const HTTP_CONTROLLER_METADATA = Symbol.for("denorid.http_controller");
-export const CONTROLLER_REQUEST_MAPPING = Symbol.for("denorid.request_mapping");
