346bb1e42de5d6435f0ed34ca3b7e6b625f9be6e
neonbyte1 <186292855+neonbyte1@users.noreply.github.com>
2026-04-06 18:41:10 +0000

    refactor(core)!: renamed exception filter related constant

--- packages/core/_constants.ts
+++ packages/core/_constants.ts
@@ -3,6 +3,6 @@ export const HTTP_CONTROLLER_METADATA = Symbol.for("denorid.http_controller");
 export const CONTROLLER_REQUEST_MAPPING = Symbol.for("denorid.request_mapping");
 
 export const EXCEPTION_FILTER = Symbol.for("denorid.exception_filter");
-export const EXCEPTION_FILTER_TARGET = Symbol.for(
-  "denorid.exception_filter.target",
+export const EXCEPTION_FILTER_METADATA = Symbol.for(
+  "denorid.exception_filter.metadata",
 );
