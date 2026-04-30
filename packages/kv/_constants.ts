2db50e1894a570677cc1d5e5213b2b50a7994a29
neonbyte1 <186292855+neonbyte1@users.noreply.github.com>
2026-04-30 21:52:06 +0000

    feat(kv): add package constants

---
+++ packages/kv/_constants.ts
@@ -0,0 +1,6 @@
+export const KV_MODULE_OPTIONS = Symbol.for("denorid.kv.module_options");
+export const QUEUE_HANDLER = Symbol.for("denorid.kv.queue_handler");
+export const QUEUE_HANDLER_METADATA = Symbol.for(
+  "denorid.kv.queue_handler.metadata",
+);
+export const DEFAULT_QUEUE_NAME = "default";
